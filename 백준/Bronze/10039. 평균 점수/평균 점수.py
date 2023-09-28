fdeed252b8713b2a051c4dd30bbb1f34e527e2cc
WinterFlw <96910692+WinterFlw@users.noreply.github.com>
2023-09-28 23:22:10 +0900

    [Bronze IV] Title: 평균 점수, Time: 48 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/10039. 평균 점수/평균 점수.py
@@ -0,0 +1,17 @@
+a = int(input())
+if a < 40:
+    a = 40
+b = int(input())
+if b < 40:
+    b = 40
+c = int(input())
+if c < 40:
+    c = 40
+d = int(input())
+if d < 40:
+    d = 40
+e = int(input())
+if e < 40:
+    e = 40
+
+print((a+b+c+d+e)//5)
