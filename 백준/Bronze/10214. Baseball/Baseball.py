15d823006469e67b096f8f7a19c2412767b08650
WinterFlw <96910692+WinterFlw@users.noreply.github.com>
2023-10-02 23:51:34 +0900

    [Bronze III] Title: Baseball, Time: 84 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/10214. Baseball/Baseball.py
@@ -0,0 +1,14 @@
+for i in range(int(input())):
+    Y = 0 
+    K = 0
+    for j in range(9):
+        y, k = map(int, input().split())
+        Y += y
+        K += k
+    
+    if Y > K:
+        print('Yonsei')
+    elif Y < K:
+        print('Korea')
+    else:
+        print("Draw")
