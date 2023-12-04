e17337333c6cdbe3a2a3ec029f311feb440c5541
WinterFlw <96910692+WinterFlw@users.noreply.github.com>
2023-12-04 14:31:33 +0000

    아니 넘파이를 왜못써...

--- 백준/test.py
+++ 백준/test.py
@@ -1,54 +1,84 @@
-import numpy as np
-
-UsedTime = 0
 y,x,inven = map(int, input().split())
 
-def counter(mapdata):
-    maxs, mins = 0, 0
-    for i in range(y):
-        for j in range(x):
-            if mapdata[i][j] == np.max(mapdata):
-                maxs += 1
-            if mapdata[i][j] == np.min(mapdata):
-                mins += 1
-    return maxs, mins
+class MineCraft:
+    def __init__(self, y, x, inven):
+        self.y = y
+        self.x = x
+        self.inven = inven
+        self.UsedTime = 0
+        self.mapdata = self.arrMake()
 
-def dig(mapdata):
-    max = np.max(mapdata)
-    for i in range(y):
-        for j in range(x):
-            if mapdata[i][j] == max:
-                mapdata[i][j] -= 1
-                UsedTime += 2
-                inven += 1
-    #최대값 다 돌면서 각각 mapdata[i][j]-=1, UsedTime += 2
+    def arrMake(self):
+        data = []
+        for i in range(y):
+            data.append([])
+            hang = map(int, input().split())
+            hang = list(hang)
+            for j in range(x):
+                data[i].append(hang[j])
+        print("\n",data)
+        return data
 
-def inv(mapdata):
-    min = np.min(mapdata)
-    for i in range(y):
-        for j in range(x):
-            if mapdata[i][j] == min:
-                mapdata[i][j] += 1
-                UsedTime -= 2
-                inven -= 1
-    #최소값 다 돌면서 각각 mapdata[i][j]+=1, UsedTime += 1
+    def realMax(self):
+        rMax = 0
+        for i in range(y):
+            if rMax < max(self.mapdata[i]):
+                rMax = max(self.mapdata[i])
+        return rMax
+    
+    def realMin(self):
+        rMin = 0
+        for i in range(y):
+            if rMin < min(self.mapdata[i]):
+                rMin = min(self.mapdata[i])
+        return rMin
 
-mapdata = np.zeros((y,x), dtype=int)
+    def counter(self):
+        maxs, mins = 0, 0
+        for i in range(y):
+            for j in range(x):
+                if self.mapdata[i][j] == self.realMax():
+                    maxs += 1
+                if self.mapdata[i][j] == self.realMin():
+                    mins += 1
+        return maxs, mins
+    
+    def dig(self):
+        maxs = self.realMax()
+        for i in range(y):
+            for j in range(x):
+                if self.mapdata[i][j] == maxs:
+                    self.mapdata[i][j] -= 1
+                    self.UsedTime += 2
+                    self.inven += 1
+                    print("Dig:",self.UsedTime, self.inven)
+        #최대값 다 돌면서 각각 mapdata[i][j]-=1, UsedTime += 2
 
-for i in range(y):
-    hang = map(int, input().split())
-    hang = list(hang)
-    for j in range(x):
-        mapdata[i][j] = hang[j]
+    def inv(self):
+        mins = self.realMin()
+        for i in range(y):
+            for j in range(x):
+                if self.mapdata[i][j] == mins:
+                    self.mapdata[i][j] += 1
+                    self.UsedTime += 1
+                    self.inven -= 1
+                    print("Inv:",self.UsedTime, self.inven)
+        #최소값 다 돌면서 각각 mapdata[i][j]+=1, UsedTime += 1
 
-while np.max(mapdata) != np.min(mapdata):
-    maxs, mins = counter(mapdata)
-    if mins > inven :
-        dig(mapdata)
-    else:
-        if maxs < mins * 2:
-            dig(mapdata)
-        else:
-            inv(mapdata)
+    def settler(self):
+        while self.realMax() != self.realMin():
+            maxs, mins = self.counter()
+            if mins > self.inven :
+                self.dig()
+            else:
+                if maxs < mins * 2:
+                    self.dig()
+                else:
+                    self.inv()
 
-print(UsedTime, mapdata[0][0])
+    def __str__(self):
+        return f"{self.UsedTime} {self.mapdata[0][0]}"
+
+mine = MineCraft(y,x,inven)
+mine.settler()
+print(mine)
